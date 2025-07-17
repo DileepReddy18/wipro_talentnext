a3a36ef8cb483b54c6cb6c043dffe0fd72efb507
DileepReddy18 <dileepreddy4765@gmail.com>
2025-07-17 10:28:46 +0530

    17/07/2025 wapper class

---
+++ wrapper_classes/src/wrapper_classes/Ques3.java
@@ -0,0 +1,12 @@
+package wrapper_classes;
+import java.util.*;
+public class Ques3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println(Integer.toBinaryString(n));
+	}
+
+}
---
+++ wrapper_classes/src/wrapper_classes/Ques4Employee.java
@@ -0,0 +1,10 @@
+package wrapper_classes;
+
+public class Ques4Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
